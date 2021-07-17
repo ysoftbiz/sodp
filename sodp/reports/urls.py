90a551f6023ca21ee35d589550bd337cd76d8c3c
calderonIrene <calderonpochirene@gmail.com>
2021-07-17 16:53:53 +0200

    REPORTS LAST CHANGES

--- sodp/reports/urls.py
+++ sodp/reports/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 
 from sodp.reports.views import (
-    #report_list_view,
+    report_list_view,
     ReportCreateView
 )
 
 app_name = "reports"
 
 urlpatterns = [
-    #path("", view=report_list_view, name="reportslist"),
+    path("", view=report_list_view, name="reportslist"),
     path('reportscreate/', ReportCreateView.as_view()),
 ]
 
