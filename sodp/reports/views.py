4ce4bfc53c512f4772628294d84684ca63126bb1
calderonIrene <calderonpochirene@gmail.com>
2021-07-17 16:12:24 +0200

    Reports last changes v2

--- sodp/reports/views.py
+++ sodp/reports/views.py
@@ -7,9 +7,6 @@ from datetime import date, timedelta
 from dateutil.relativedelta import relativedelta
 from reports.forms import ReportCreateForm
 
-
-
-
 class ReportListView(generic.ListView):
     model = report
     context_object_name = 'reportsList'
@@ -18,75 +15,29 @@ class ReportListView(generic.ListView):
     
 report_list_view = ReportListView.as_view()
 
-    #model = report
-    #template_name = 'reportscreate.html'
-    #fields = ['name','project','dateFrom','dateTo']
-
-
-    #model = report
-    #form_class = ReportCreateForm
-    #success_url = '/home/'
-    #template_name = 'reports/reportscreate.html'
-    #fields = ('name','project','dateFrom','dateTo')
-
-    #def get(self, request, *args, **kwargs):
-    #    context = {'form': ReportCreateForm()}
-    #    return render(request, 'reports/reportscreate.html', context)
-
-  
-
-
-    #def post(self, request, *args, **kwargs):
-        #form = ReportCreateForm(request.POST)
-    #    if form.is_valid():
-        #    report = form.save()
-        #    report.save()
-    #        self.object = form.save(commit=False)
-            #self.object.user_id = self.request.user.id
-    #        self.object.save()
-    #        return super(ReportCreateView, self).form_valid(form)
-
-            #return HttpResponseRedirect(reverse_lazy('report:detail', args=[report.id]))
-        #return render(request, 'reports/reportscreate.html', {'form': form})
-
-
-
-
 class ReportCreateView(CreateView):
     template_name = 'reports/reportscreate.html'
     form_class = ReportCreateForm
+    success_url = '../../reportslist'
 
     def get_initial(self):
         super(ReportCreateView, self).get_initial()
-        date_format = '%d/%m/%Y'
+        #date_format = '%d/%m/%Y'
   
         auxDateTo = date.today() - timedelta(1)
 
         n = 1
         auxDateFrom = auxDateTo - relativedelta(months=n)
 
-        self.initial = {"dateFrom":auxDateFrom.strftime(date_format), "dateTo":auxDateTo.strftime(date_format)}
+        self.initial = {"dateFrom":auxDateFrom, "dateTo":auxDateTo}
         return self.initial
-
-
 
     def post(self, request, *args, **kwargs):
         form = ReportCreateForm(request.POST)
         if form.is_valid():
-            report = form.save()
+            report = form.save(commit=False)
+            report.user = request.user
             report.save()
-            return HttpResponseRedirect(reverse_lazy('report:detail', args=[book.id]))
-        return render(request, 'reports/reportscreate.html', {'form': form})
-
-    #def post(self, request, *args, **kwargs):
-        #form = ReportCreateForm(request.POST)
-    #    if form.is_valid():
-        #    report = form.save()
-        #    report.save()
-    #        self.object = form.save(commit=False)
-            #self.object.user_id = self.request.user.id
-    #        self.object.save()
-    #        return super(ReportCreateView, self).form_valid(form)
-
-            #return HttpResponseRedirect(reverse_lazy('report:detail', args=[report.id]))
+            #return HttpResponseRedirect(reverse_lazy('report:detail', args=[book.id]))
         #return render(request, 'reports/reportscreate.html', {'form': form})
+        return super(ReportCreateView,self).form_valid(form)
