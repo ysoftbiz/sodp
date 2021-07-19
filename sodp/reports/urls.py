63a390e0ecb42a7e5eb659cff43003c16d3bab3e
calderonIrene <calderonpochirene@gmail.com>
2021-07-19 17:22:24 +0200

    Reports last last changes

--- sodp/reports/urls.py
+++ sodp/reports/urls.py
@@ -8,7 +8,7 @@ from sodp.reports.views import (
 app_name = "reports"
 
 urlpatterns = [
-    path("", view=report_list_view, name="reportslist"),
+    path("reportslist/", view=report_list_view, name="reportslist"),
     path('reportscreate/', ReportCreateView.as_view()),
 
 ]
