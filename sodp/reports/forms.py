2121f6a5ea1192481f81d2ce588be76872e03ec8
calderonIrene <calderonpochirene@gmail.com>
2021-07-20 15:37:40 +0200

    Datepicker added

--- sodp/reports/forms.py
+++ sodp/reports/forms.py
@@ -1,7 +1,13 @@
 from django.forms import ModelForm
+from django.forms import DateInput
 from sodp.reports.models import report
+
+
+class DateInput(DateInput):
+    input_type = 'date'
 
 class ReportCreateForm(ModelForm):
     class Meta(object):
         model = report
-        fields = ('name','project', 'sitemap', 'dateFrom','dateTo')
+        fields = ('name','project', 'sitemap', 'dateFrom' ,'dateTo')
+        widgets = {'dateFrom' : DateInput() ,'dateTo' : DateInput()}
