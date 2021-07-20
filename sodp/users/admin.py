8c10dc709ad75d2f2e0868ceef194ec1fb5ebe5f
calderonIrene <calderonpochirene@gmail.com>
2021-07-20 13:45:23 +0200

    Tokens null value allowed

--- sodp/users/admin.py
+++ sodp/users/admin.py
@@ -28,6 +28,7 @@ class UserAdmin(auth_admin.UserAdmin):
             },
         ),
         (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+        (_("Tokens"), {"fields": ("ahrefs_token", "google_token")}),
     )
     list_display = ["username", "name", "is_superuser"]
     search_fields = ["name"]
