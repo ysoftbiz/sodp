02ed24e7d1d391cf8cfd14ff063b7a1ed69bc29d
Yolanda Robla <info@ysoft.biz>
2021-08-12 09:24:51 +0200

    Add fixes in ahrefs extraction

--- sodp/utils/ahrefs.py
+++ sodp/utils/ahrefs.py
@@ -11,7 +11,7 @@ def getAhrefsInfo(token, url):
     try:
         r = requests.get(ahrefs_url)
         data = json.loads(r.content.decode('utf-8'))
-        pages = data['pages']
+        pages = data.get('pages', [])
         if len(pages)>0:
             return pages[0].get('backlinks', 0)
     except Exception as e:
